317bd5d01396825867f2df06f6d784ede2903ab3
Dr Nic Williams <drnicwilliams@gmail.com>
2012-11-13 22:41:27 -0800

    request_stager_to_stage_app outputs the stager logs when complete

--- apps/staging_client_service/app.rb
+++ apps/staging_client_service/app.rb
@@ -2,12 +2,13 @@ require 'sinatra'
 
 get '/download_unstaged_app/:id' do
   p params
-  unstaged_app_tgz = File.expand_path("../../sinatra-app.tgz", __FILE__)
+  unstaged_app_tgz = File.expand_path("../../sinatra-app.zip", __FILE__)
   send_file(unstaged_app_tgz)
 end
 
 post '/upload_droplet/:id/:upload_id' do
   p params
-  src_path = params[:upload][:droplet].path
+  src_path = params[:upload][:droplet][:tempfile]
+  droplet = params[:upload][:droplet][:tempfile].read
   puts "stager uploaded staged droplet #{src_path}"
 end
