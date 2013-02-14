9919b5c51e5b23561d2fdd8163d4f9fa0d857e2b
Dr Nic Williams <drnicwilliams@gmail.com>
2013-02-14 11:32:47 -0800

    Extra print statements to see client/cloud_controller working

--- apps/staging_client_service/app.rb
+++ apps/staging_client_service/app.rb
@@ -1,14 +1,16 @@
 require 'sinatra'
 
 get '/download_unstaged_app/:id' do
+  puts "UNSTAGED APP BEING REQUESTED"
   p params
   unstaged_app_tgz = File.expand_path("../../sinatra-app.zip", __FILE__)
   send_file(unstaged_app_tgz)
 end
 
 post '/upload_droplet/:id/:upload_id' do
+  puts "RECEIVING DROPLET"
   p params
   src_path = params[:upload][:droplet][:tempfile]
   droplet = params[:upload][:droplet][:tempfile].read
-  puts "stager uploaded staged droplet #{src_path}"
+  puts "RECEIVED DROPLET: stager uploaded staged droplet #{src_path}"
 end
